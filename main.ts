44af24800d127e7a5b8f401d883e88684423ecc7
Pure1000 <purebionest@gmail.com>
2025-04-16 20:48:42 +0200

    extend

--- main.ts
+++ main.ts
@@ -1,8 +1,20 @@
 input.onButtonPressed(Button.A, function () {
     basic.showIcon(IconNames.Happy)
 })
+input.onButtonPressed(Button.AB, function () {
+    basic.showIcon(IconNames.Surprised)
+})
 input.onButtonPressed(Button.B, function () {
     basic.showIcon(IconNames.Sad)
+})
+input.onGesture(Gesture.Shake, function () {
+    basic.showLeds(`
+        . # . # .
+        . # . # .
+        . . . . .
+        # . # . #
+        . # . # .
+        `)
 })
 basic.forever(function () {
 	
